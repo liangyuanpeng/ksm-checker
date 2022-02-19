2fcaf54bd7e915f4e564cc17f4f769e439639ebd
LanLiang <liangyuanpengem@163.com>
2022-02-19 16:39:35 +0800

    Add Dockerfile and finish compare metrics data and k8s cluster data

--- main.go
+++ main.go
@@ -3,18 +3,82 @@ package main
 import (
 	"bytes"
 	"context"
+	"flag"
 	"io"
 	"log"
 	"net/http"
+	"path/filepath"
 	"strings"
 	"time"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
 )
 
 func main() {
-	result, code := get(context.TODO(), "http://localhost:8080/metrics")
+
+	var kubeconfig *string
+	var url *string
+
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	url = flag.String("url", "", "metrics url ")
+
+	flag.Parse()
+
+	if *url == "" {
+		log.Fatalln("set metrics url with --url")
+	}
+
+	// use the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		log.Printf("use kubeconfig with failed |%s| and try to run with inCluster\n", err.Error())
+		config, err = rest.InClusterConfig()
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	result, code := get(context.TODO(), *url)
 	if code == 200 {
 		datamap := parseMetricsForPod(result)
-		log.Println("datamap:", datamap)
+		// log.Println("datamap:", datamap)
+		for ns, mpods := range datamap {
+			if len(mpods) > 0 {
+				log.Printf("select pod from namespace:%s\n", ns)
+				pods, err := clientset.CoreV1().Pods(ns).List(context.TODO(), metav1.ListOptions{})
+				if err != nil {
+					panic(err)
+				}
+
+				apidataMap := make(map[string]int)
+				for i := 0; i < len(pods.Items); i++ {
+					apidataMap[pods.Items[i].Name] = 0
+				}
+
+				for i := 0; i < len(mpods); i++ {
+					if _, ok := apidataMap[mpods[i]]; !ok {
+						log.Printf("this pod exist in ksm and not exist k8s cluster:%s\n", mpods[i])
+					}
+				}
+
+				// log.Println("pods.pods:", len(pods.Items))
+			}
+		}
+	} else {
+		log.Fatalf("get metrics from %s failed!", url)
 	}
 }
 
@@ -35,10 +99,10 @@ func parseMetricsForPod(metrics string) map[string][]string {
 				for j := 0; j < len(keyvalues); j++ {
 					kv := strings.Split(keyvalues[j], "=")
 					if kv[0] == "namespace" {
-						namespace = kv[1]
+						namespace = strings.ReplaceAll(kv[1], "\"", "")
 					}
 					if kv[0] == "pod" {
-						pod = kv[1]
+						pod = strings.ReplaceAll(kv[1], "\"", "")
 					}
 					if namespace != "" && pod != "" {
 						break
